8f277022ceb5652553d303caadb9e4a70df7f781
Wishvee Tharu <104784856+WishveeTharu@users.noreply.github.com>
2023-02-08 15:10:15 +0530

    23.02.08.1.3.09 Delete Com

---
+++ php/delete.php
@@ -0,0 +1,17 @@
+<?php
+include "dbcon.php";
+include "encryption.php";
+
+function deleteData($conn,$uid){
+    $sql = "DELETE FROM user_accounts WHERE uid= '$uid'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+      } else {
+        echo "Error deleting record: " . $conn->error;
+      }
+}
+
+
+
+?>
