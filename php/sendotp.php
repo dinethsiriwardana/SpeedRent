0e743a71933e036a9ec193b88a236659a7ba123d
HiruDilmi <117562801+HiruDilmi@users.noreply.github.com>
2023-02-08 15:52:19 +0530

    Create sendotp.php

---
+++ php/sendotp.php
@@ -0,0 +1,39 @@
+<?php
+    include "dbcon.php";
+    include "encryption.php";
+
+    session_start();
+
+    function sendotp($conn)
+    {
+
+        $table = "user_accounts";
+        $where = "email = 'hirudilmih@gmail.com'";
+        $select = "*";
+
+        // $sql = "SELECT $select FROM $table where $where";
+        // $result = $conn->query($sql);
+
+        // if ($result->num_rows == 1)
+        // {
+            $otp = random_int(100000, 999999);
+            date_default_timezone_set('Asia/Colombo');
+            $date = date('Y-m-d h:i:s');
+            echo $date;
+            $updatesql = "UPDATE user_accounts SET otp = $otp, otp_time = '$date'  WHERE $where";
+            if($conn -> query($updatesql) === TRUE)
+            {
+                echo "Record updated successfully";
+            }else{
+                echo "Error updating record ". $conn -> error;
+            }
+        // }
+        // else{
+        //     echo "E mail does not exists...!";
+        // }
+
+  
+    }
+    sendotp($conn);
+    $conn->close();   
+?>
