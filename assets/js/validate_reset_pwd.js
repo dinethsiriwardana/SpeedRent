4b7097137b538cb2627ee9740e43e94e1d1558da
HiruDilmi <117562801+HiruDilmi@users.noreply.github.com>
2023-02-21 18:59:59 +0530

    23.02.21.1.6.59 validate_reset_pwd

--- assets/js/validate_reset_pwd.js
+++ assets/js/validate_reset_pwd.js
@@ -1,3 +1,14 @@
-var pwd = document.getElementById("pwd");
-var cnfrmpwd = document.getElementById("cnfrmpwd");
-
+function checkpwds(){
+    var pwd = document.getElementById("pwd").value;
+    var cnfrmpwd = document.getElementById("cnfrmpwd").value;
+    
+    if(pwd != cnfrmpwd)
+    {
+        window.alert("Passwords do not match...!!!");
+        return false;
+    }
+    else{
+        window.alert("Registered succesfully...!!!");
+       
+    }
+}
