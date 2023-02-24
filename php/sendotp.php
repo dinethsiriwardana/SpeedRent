36e014ff433beb5b1a245ea68e7abd68111b4dea
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-02-24 13:51:46 +0530

    23.02.24.1.1.51

--- php/sendotp.php
+++ php/sendotp.php
@@ -1,32 +1,17 @@
 <?php
     include "dbcon.php";
     include "encryption.php";
-    include "php/phpmailer/sendMail.php";
+    include "phpmailer/sendMail.php";
 
-<<<<<<< HEAD
-    
-=======
-    function sendotp($conn)
-    {
-        $email = $_POST['email'];
->>>>>>> DinethS
+
+
+        session_start();
 
         $email = $_POST['email'];
         $table = "user_accounts";
         $where = "email = '$email'";
         $select = "*";
 
-<<<<<<< HEAD
-=======
-        // $sql = "SELECT $select FROM $table where $where";
-        // $result = $conn->query($sql);
-
-
-        
-
-        // if ($result->num_rows == 1)
-        // {
->>>>>>> DinethS
             $otp = random_int(100000, 999999);
             date_default_timezone_set('Asia/Colombo');
             $date = date('Y-m-d h:i:s');
@@ -34,22 +19,18 @@
             $updatesql = "UPDATE user_accounts SET otp = $otp, otp_time = '$date'  WHERE $where";
             if($conn -> query($updatesql) === TRUE)
             {
-                // sendMail($subject,$body,$receiver);
+                setcookie('OTP', encryption($otp), time() + 300, "/");
+                sendMail('shadow8netsw@gmail.com');
+
+                
               
 
             }else{
                 echo "Error updating record ". $conn -> error;
             }
-<<<<<<< HEAD
+
  
- 
-=======
-        // }
-        // else{
-        //     echo "E mail does not exists...!";
-        // }
-    }
-    sendotp($conn);
->>>>>>> DinethS
+    
+
     $conn->close();   
 ?>
