d080e3c8c7f4624bdb431754134e52449ae02c83
Hasitha Sandeepa <hasithasandeepa2000@gmail.com>
2023-03-02 01:22:42 +0530

    Add new files to php
    23.03.02.01.22.AM

---
+++ php/changeOrderStatus.php
@@ -0,0 +1,16 @@
+<?php
+    include 'dbcon.php';
+
+    $status = 'completed';
+    $orderId = 20001;
+
+    $sql = "UPDATE rent_order SET stts = '$status' WHERE orderid = $orderId";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "order record updated successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
