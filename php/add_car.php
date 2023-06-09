ed42cab6a9159a621e6e1d6a43067079d0476f09
himashagimhani <gimhanigalbadage@gmail.com>
2023-06-09 13:37:06 +0530

    Update add_car.php
    add_car

--- php/add_car.php
+++ php/add_car.php
@@ -1,28 +1,21 @@
 <?php
-$target_dir = "assets/img/cars";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    include 'dbcon.php';
 
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-  
-    //  // Check if $uploadOk is set to 0 by an error
-//   if ($uploadOk == 0) {
-//       echo "Sorry, your file was not uploaded.";
-    // // if everything is ok, try to upload file
-//     } else {
-//         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-//             echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-//         } else {
-//             echo "Sorry, there was an error uploading your file.";
-//         }
-//     }
+    $cid = 2;
+    $cName = 'Honda';
+    $cModel = 'Grace';
+    $cDetails = 'Petrol';
+    $cPrice = 2000.0;
+    $avb = 0;
 
-    ?>
+    $sql = "INSERT INTO item_table (carid, car_name, car_model, details, price, availability)
+        VALUES ('$cid', '$cName', '$cModel', '$cDetails', '$cPrice', '$avb')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
