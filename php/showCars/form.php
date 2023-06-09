a0f00e06ff03f41e16834a0cba252c4272ab83d1
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2023-06-09 10:33:20 +0530

    updated

---
+++ php/showCars/form.php
@@ -0,0 +1,34 @@
+<div class="col-lg-3 col-xl-3 col-xxl-2">
+    <div style="border-radius: 10px;box-shadow: 0px 0px 14px 0px rgba(33,37,41,0.04);border-style: solid;border-color: var(--bs-gray-200);padding-right: 20px;padding-left: 20px;color: rgba(33,37,41,0.8);padding-bottom: 22px;margin-bottom: 50px;">
+        <h1 style="text-align: center;font-size: 30px;margin-top: 30px;color: var(--bs-red);">Filters</h1>
+        <hr>
+        <div class="col">
+            <small style="font-size: 25px;">Brand</small>
+            <div class="form-check" style="margin-bottom: 5px;margin-top: 20px;">
+                <input class="form-check-input" type="checkbox" id="formCheck-1">
+                <label class="form-check-label" for="formCheck-1">AUDI</label>
+            </div>
+            <div class="form-check" style="margin-bottom: 5px;">
+                <input class="form-check-input" type="checkbox" id="formCheck-4">
+                <label class="form-check-label" for="formCheck-4">Vitz</label>
+            </div>
+            <div class="form-check" style="margin-bottom: 5px;">
+                <input class="form-check-input" type="checkbox" id="formCheck-3">
+                <label class="form-check-label" for="formCheck-3">BMW</label>
+            </div>
+            <div class="form-check" style="margin-bottom: 5px;">
+                <input class="form-check-input" type="checkbox" id="formCheck-2">
+                <label class="form-check-label" for="formCheck-2">Passo</label>
+            </div>
+        </div>
+        <hr>
+        <div class="col">
+            <small style="font-size: 25px;">Price</small>
+        </div>
+        <div class="col">
+            <input class="form-control-sm" type="number" name="minprice" min="0" step="10" placeholder="Max" style="width: 100%;height: 37px;border-color: rgba(0,0,0,0.13);border-radius: 5px;margin-top: 10px;margin-left: 0px;">
+            <input class="form-control-sm" type="number" name="minprice" min="0" step="10" placeholder="Min" style="width: 100%;height: 37px;border-color: rgba(0,0,0,0.13);border-radius: 5px;margin-top: 20px;margin-right: 18px;">
+            <button class="btn btn-primary" style="background: var(--bs-red);border-style: none;margin-top: 15px;width: 100%;">Search</button>
+        </div>
+    </div>
+</div>
